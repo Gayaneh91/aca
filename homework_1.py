b89ca77a6b2b291b30f032c40ce9cb41985cf34a
Gayane <gayanehgrig@gmail.com>
2022-06-09 13:48:30 +0400

    Added homework

--- homework_1.py
+++ homework_1.py
@@ -3,45 +3,94 @@
 # from money import Money
 # from date_time import DateTime
 import random
-
-from classwork.date_time import dt
+from datetime import date
+import string
+from classwork.date_time import Date, d
 from classwork.money import m
 from classwork.person import p
 
 
 class BankAccount:
-    def __init__(self, p : 'Person', m: 'Money', d: 'DateTime'): #-> None
-        self.customer = p
-        self.account_number = [random.randint(0, 50) for _ in range(16)]   #TODO: generate random 16-len string from digits
-        self.balance = m
-        self.valid_till = d
+    def __init__(self, p : 'Person', m: 'Money', d: 'Date') -> None:
+        self.__customer = p
+        self.__account_number = ''.join(random.choices(string.digits, k = 16))
+        self.__balance = m
+        self.__valid_till = d
         self.m = None
         self.d = None
         self.p = None
         #TODO: add other data members if you need
 
+
+
     def __str__(self):
-        return f'{self.customer}\nCard number - {self.account_number}\nAccount balance - {self.balance}' \
-               f'\nValid till - {self.valid_till}'
+        return f'{self.__customer}\nCard number - {self.__account_number}\nAccount balance - {self.__balance}' \
+               f'\nValid till - {self.__valid_till}'
+    def get__customer(self):
+         return self.__customer
+
+    def get__account_number(self):
+        return self.__account_number
+
+    def get__balance(self):
+        return self.__balance
+
+    def get__valid_till(self):
+        return self.__valid_till
+
+    def set__customer(self, x):
+        self.__customer = p
+
+    def set__account_number(self, x):
+        self.__account_number = x
+
+    def set__balance(self, m):
+        self.__balance = m
+
+    def set__valid_till(self, d):
+        self.__valid_till = d
+
+
+
 
     def deal(self, m):
         #TODO: make transaction , check if balance is enough
-        if self.balance > 0:
-            self.balance -= self.m
+        x = self.__balance - m
+
+        if x:
+            self.__balance = x
+            print('Deal approved. Your current balance is' '-' f'{x}')
+        else:
+            print('Sorry:( Balance is not enough.')
 
     def fill_balance(self, m):
         #TODO: fill your balance
-        self.balance += m
+        self.__balance += m
+
+        print('Congratulations! Balance successfully filled. Your current balance is' '-' f'{self.__balance}')
 
     def deposite(self, m, d, p):
         # money, duration, percent
         # TODO: count money after duration with p percent
-        x = self.balance - self.m
-        self.d = x + self.m
+        x_1 = self.balance - self.m
+        self.d = x_1 + self.m
         self.p = (self.d / self.balance) * 100
 
-ba = BankAccount(p, m, dt)
-# ba.deposite(200, 6, 9)
-print(ba)
-print("aaaa")
+    def update(self, n):
+        self.__valid_till.add_year(2)
+
+
+ba = BankAccount(p, m, d)
+# print(ba)
+ba.set__account_number(x)  #Գեղամ ջան այստեղ որպես պարամետր ի՞նչ պիտի փողանցենք։
+print(ba.get__account_number())
+# ba.fill_balance(m)
+# vt = Date(2024, 6, 8)
+# ba.update(vt)
+# m_1 = ('USD', 2000)
+# ba.deal(m_1)
+# ba.fill_balance(m)
+# ba.deposite(600, 3, 60)
+
+
 
